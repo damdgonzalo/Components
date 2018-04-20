cf5e68cdd4ae89afb9b34f01cf6a9ae5f5533fa2
damdgonzalo <danamg_@hotmail.com>
2018-04-20 15:58:13 +0200

    Notes

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,47 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.List;
+
 
 public class Main {
-
+	private Connection conn;
+	private Statement stmt;
+	
+	private int comptadorNotes;
+	
+	public Main(Connection conn, Statement stmt) throws Exception {
+		this.conn = conn;
+		this.stmt = stmt;
+	
+		comptarNotesInicials();
+	}
+	
+//----------------------------------------------------------------------------------------------------------------------
+	
+	public boolean hiHaNotesNoves() throws Exception {
+		stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT COUNT(*) AS \"comptNotes\"FROM \"Notes\"");
+		rs.next();
+		
+		int comptNotesNou = rs.getInt("comptNotes");
+		
+		if (comptNotesNou > comptadorNotes) {
+			comptadorNotes = comptNotesNou;
+			return true;
+		}
+		
+		return false;
+	}
+	
+	
+	
+	private void comptarNotesInicials() throws Exception {
+		stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT COUNT(*) AS \"comptNotes\"FROM \"Notes\"");
+		rs.next();
+		
+		comptadorNotes = rs.getInt("comptNotes");
+	}
 }
---
+++ src/Nota.java
@@ -0,0 +1,66 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Nota {
+
+	private String titol;
+	private String text;
+	private String dataPublicacio;
+	private String dataUltModificacio;
+	private String autor;
+	private List<String> etiquetes;
+	
+	public Nota() {
+		etiquetes = new LinkedList<>();
+	}
+
+	public String getTitol() {
+		return titol;
+	}
+
+	public void setTitol(String titol) {
+		this.titol = titol;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getDataPublicacio() {
+		return dataPublicacio;
+	}
+
+	public void setDataPublicacio(String dataPublicacio) {
+		this.dataPublicacio = dataPublicacio;
+	}
+
+	public String getDataUltModificacio() {
+		return dataUltModificacio;
+	}
+
+	public void setDataUltModificacio(String dataUltModificacio) {
+		this.dataUltModificacio = dataUltModificacio;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public List<String> getEtiquetes() {
+		return etiquetes;
+	}
+
+	public void setEtiquetes(List<String> etiquetes) {
+		this.etiquetes = etiquetes;
+	}
+	
+	x
+}
