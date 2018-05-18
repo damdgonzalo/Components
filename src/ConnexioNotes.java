4fbd05fad9b9b0cc4862e36ec98680cb6a16c15b
Xavier Castillo <xcastillo.girosystem@gmail.com>
2018-05-18 12:07:40 +0200

    Commit
    Inserir noves notes.

--- src/ConnexioNotes.java
+++ src/ConnexioNotes.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.LinkedList;
 import java.util.List;
@@ -47,38 +48,37 @@ import java.util.List;
 public class ConnexioNotes implements IConnexioNotes{
 	private Connection conn;
 	private Statement stmt;
-	
+
 	private int comptadorNotes;
-	private int comptadorNotesNoves;
-	
+	private int comptadorNotesVell;
+
 	private List<String> grups;
+	private List<String> grupsID;
 	private String usuariConnectat;
-	
+
 	/**
 	 * Constructor
 	 * @param conn Connexió amb la base de dades
 	 * @param grups Llista amb els noms dels grups als que pertany l'usuari
 	 * @throws Exception
 	 */
-	/*public ConnexioNotes(Connection conn, List<String> grups) throws Exception {
+	public ConnexioNotes(Connection conn, List<String> grups) throws Exception {
 		this.conn = conn;
 		this.grups = grups;
-	
+
 		comptadorNotes = comptarNotesActuals();
-		comptadorNotesNoves = 0;
-	}*/
-	
+		comptadorNotesVell = comptadorNotes;
+	}
+
 	public ConnexioNotes(Connection conn, String usuariConnectat) throws Exception {
 		this.conn = conn;
 		this.usuariConnectat = usuariConnectat;
 		grups = getGrupsUsuari();
-		
-		comptadorNotes = comptarNotesActuals();
-		comptadorNotesNoves = 0;
+		grupsID = getGrupsUsuariID();
 	}
-	
-//----------------------------------------------------------------------------------------------------------------------
-	
+
+	//----------------------------------------------------------------------------------------------------------------------
+
 	/**
 	 * Retorna tota la informació d'una nota donada la seva ID
 	 * @param idNota ID de la nota que es vol
@@ -86,44 +86,46 @@ public class ConnexioNotes implements IConnexioNotes{
 	 */
 	public Nota getNota(String idNota) throws Exception {
 		String query = "SELECT * FROM \"Notes\" WHERE \"idNota\"='" + idNota + "'";
-		
+
 		stmt = conn.createStatement();
 		ResultSet rs = stmt.executeQuery(query);
 		rs.next();
-		
+
 		Nota nota = new Nota();
 		nota.setTitol(rs.getString("titol"));
 		nota.setDataPublicacio(rs.getString("dataCreacio"));
 		nota.setDataUltModificacio(rs.getString("ultimaModificacio"));
 		nota.setText(rs.getString("cos"));
 		nota.setAutor(rs.getString("autor"));
-		
+
 		return nota;
 	}
-	
-//----------------------------------------------------------------------------------------------------------------------
+
+	//----------------------------------------------------------------------------------------------------------------------
 
 	/**
 	 * Retorna una llista amb totes les notes que hi ha als grups que pertany l'usuari
- 	 * @return Llista de notes
+	 * @return Llista de notes
 	 * @throws Exception
 	 */
 	public List<Nota> getNotes() throws Exception {
-				
+
 		String query = "SELECT g.\"idGrup\", n.* FROM \"GrupsNota\" g";
-		  query += " INNER JOIN \"Notes\" n ON g.\"idNota\" = n.\"idNota\" WHERE ";
-	
+		query += " INNER JOIN \"Notes\" n ON g.\"idNota\" = n.\"idNota\" WHERE ";
+
 		for (int i=0; i<grups.size(); i++) {
-			
+
 			if (i==grups.size()-1) query += "g.\"idGrup\" = (SELECT \"idGrup\" FROM \"Grups\" WHERE \"nom\"='" + grups.get(i) + "')";
 			else query += "g.\"idGrup\" = (SELECT \"idGrup\" FROM \"Grups\" WHERE \"nom\"='" + grups.get(i) + "') OR ";
 		}
-			
+
+		//System.out.println(query);
+
 		stmt = conn.createStatement();
 		ResultSet rs = stmt.executeQuery(query);
-		
+
 		List<Nota> llistaNotes = new LinkedList<>();
-		
+
 		while (rs.next()) {
 			Nota nota = new Nota();
 			nota.setTitol(rs.getString("titol"));
@@ -131,16 +133,16 @@ public class ConnexioNotes implements IConnexioNotes{
 			nota.setDataUltModificacio(rs.getString("ultimaModificacio"));
 			nota.setText(rs.getString("cos"));
 			nota.setAutor(rs.getString("autor"));
-			
-			
+
+
 			llistaNotes.add(nota);
 		}
-		
+
 		return llistaNotes;
 	}
-	
-//----------------------------------------------------------------------------------------------------------------------
-	
+
+	//----------------------------------------------------------------------------------------------------------------------
+
 	/**
 	 * Retorna una llista nom�s amb les notes noves que s'han penjat als grups que pertany l'usuari.
 	 * Si no hi ha cap nota nova, retorna una llista buida.
@@ -149,26 +151,26 @@ public class ConnexioNotes implements IConnexioNotes{
 	 */
 	public List<Nota> getNotesNoves() throws Exception {
 		String query = "SELECT g.\"idGrup\", n.* FROM \"GrupsNota\" g";
-			  query += " INNER JOIN \"Notes\" n ON g.\"idNota\" = n.\"idNota\" WHERE ";
-		
+		query += " INNER JOIN \"Notes\" n ON g.\"idNota\" = n.\"idNota\" WHERE ";
+
 		//busca notes noves a cada grup
 		for (int i=0; i<grups.size(); i++) {
-			
+
 			if (i==grups.size()-1) query += "g.\"idGrup\" = (SELECT \"idGrup\" FROM \"Grups\" WHERE \"nom\"='" + grups.get(i) + "')";
 			else query += "g.\"idGrup\" = (SELECT \"idGrup\" FROM \"Grups\" WHERE \"nom\"='" + grups.get(i) + "') OR ";
 		}
-		
-		query += " ORDER BY n.\"idNota\" DESC LIMIT " + (comptadorNotesNoves);
-		
+
+		query += " ORDER BY n.\"idNota\" DESC LIMIT " + (comptarNotesActuals() - comptadorNotesVell);
+
 		stmt = conn.createStatement();
 		ResultSet rs = stmt.executeQuery(query);
-		
-		
+
+
 		List<Nota> llistaNotesNoves = new LinkedList<>();
-		
-		comptadorNotes += comptadorNotesNoves;
-		
-		comptadorNotesNoves = 0;
+
+		comptadorNotesVell = comptadorNotes;
+
+		comptadorNotes = 0;
 		while (rs.next()) {
 			Nota notaNova = new Nota();
 			notaNova.setTitol(rs.getString("titol"));
@@ -176,58 +178,57 @@ public class ConnexioNotes implements IConnexioNotes{
 			notaNova.setDataUltModificacio(rs.getString("ultimaModificacio"));
 			notaNova.setText(rs.getString("cos"));
 			notaNova.setAutor(rs.getString("autor"));
-			
+
 			llistaNotesNoves.add(notaNova);
+			comptadorNotes++;
 		}
-		
+
 		return llistaNotesNoves;
 	}
-	
-//----------------------------------------------------------------------------------------------------------------------
-	
+
+	//----------------------------------------------------------------------------------------------------------------------
+
 	/**
 	 * Comprova si hi ha notes noves en algun dels grups als que pertany l'usuari
 	 * @return TRUE si l'usuari t� notes noves
 	 */
-	public boolean hiHaNotesNoves() throws Exception {
-		int comptadorAux = comptarNotesActuals();
-		if (comptadorAux != 0) {
-			if (comptadorAux > comptadorNotes) {
-				comptadorNotesNoves = comptadorAux - comptadorNotes;
-				return true;
-			}
+	public boolean hiHaNotesNoves() throws Exception {		
+		int notesActuals = comptarNotesActuals();
+
+		if (notesActuals != 0) {
+			if (notesActuals > comptadorNotes) return true;
 		}
-		
+
 		return false;
 	}
-	
-	
-//----------------------------------------------------------------------------------------------------------------------
-	
+
+
+	//----------------------------------------------------------------------------------------------------------------------
+
 	/**
 	 * Compta les notes que hi ha a la base de dades als grups als que pertany l'usuari
 	 * @return Número de notes que hi ha en un moment determinat
 	 */
 	public int comptarNotesActuals() throws Exception {
 		String query = "SELECT COUNT(*) AS \"comptNotes\" FROM \"GrupsNota\" WHERE ";
-		
+
 		for (int i=0; i<grups.size(); i++) {
-			
+
 			if (i==grups.size()-1) query += "\"idGrup\" = (SELECT \"idGrup\" FROM \"Grups\" WHERE \"nom\"='" + grups.get(i) + "')";
 			else query += "\"idGrup\" = (SELECT \"idGrup\" FROM \"Grups\" WHERE \"nom\"='" + grups.get(i) + "') OR ";
 		}
-		
+
 		stmt = conn.createStatement();
 		ResultSet rs = stmt.executeQuery(query);
 		rs.next();
-		
-		int notesActuals = rs.getInt("comptNotes");
-		
-		return notesActuals;
+
+		int notesInicials = rs.getInt("comptNotes");
+
+		return notesInicials;
 	}
-	
-//----------------------------------------------------------------------------------------------------------------------
-	
+
+	//----------------------------------------------------------------------------------------------------------------------
+
 
 	/**
 	 * Retorna una llista amb els noms dels grups als quals pertany l'usuari connectat
@@ -235,17 +236,132 @@ public class ConnexioNotes implements IConnexioNotes{
 	 */
 	public List<String> getGrupsUsuari() throws Exception {
 		String query = "SELECT gr.\"nom\" FROM \"Grups\" gr"
-					 + " INNER JOIN \"GrupsUsuaris\" gu ON gr.\"idGrup\"=gu.\"idGrup\""
-					 + " WHERE gu.\"idUsuari\"='" + usuariConnectat + "'";
-		
+				+ " INNER JOIN \"GrupsUsuaris\" gu ON gr.\"idGrup\"=gu.\"idGrup\""
+				+ " WHERE gu.\"idUsuari\"='" + usuariConnectat + "'";
+
 		stmt = conn.createStatement();
 		ResultSet rs = stmt.executeQuery(query);
-		
+
 		List<String> grups = new LinkedList<>();
 		while (rs.next()) grups.add(rs.getString("nom"));
-		
-		
+
 		return grups;
+	}
+
+	//----------------------------------------------------------------------------------------------------------------------
+
+	/**
+	 * Retorna l'ultima nota que ha estat entrada.
+	 * @return cadena de text amb id de la nota.
+	 */
+	public String getUltimaNota() throws Exception {
+		String query = "SELECT \"idNota\" FROM \"Notes\" ORDER BY \"idNota\" DESC LIMIT 1";
+
+		stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery(query);
+		if (!rs.next()) {
+			System.out.println("no hi ha notes");
+		}
+		String nota = rs.getString("idNota");
+
+		return nota;
+	}
+	
+	//----------------------------------------------------------------------------------------------------------------------
+	
+	/**
+	 * Retorna una llista amb les IDs dels grups als quals pertany l'usuari connectat
+	 * @return Llista amb IDs de grups existents als que pertany l'usuari
+	 */
+	public List<String> getGrupsUsuariID() throws Exception {
+		String query = "SELECT gr.\"idGrup\" FROM \"Grups\" gr"
+				+ " INNER JOIN \"GrupsUsuaris\" gu ON gr.\"idGrup\"=gu.\"idGrup\""
+				+ " WHERE gu.\"idUsuari\"='" + usuariConnectat + "'";
+
+		stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery(query);
+
+		List<String> grups = new LinkedList<>();
+		while (rs.next()) grups.add(rs.getString("idGrup"));
+
+		return grups;
+	}
+	
+	//----------------------------------------------------------------------------------------------------------------------
+	
+	/**
+	 * Insereix a la base de dades una nota.
+	 * @param n
+	 * @param nom
+	 * @param dataCreacio
+	 * @param ultimaModificacio
+	 * @param cos
+	 * @param author
+	 * @return
+	 * @throws Exception
+	 */
+	public String inserirNovaNota(int n,String nom, String dataCreacio, String ultimaModificacio, String cos, String author) throws Exception {
+		try
+		{
+			conn.setAutoCommit(false);
+			stmt = conn.createStatement();
+			stmt.executeUpdate("INSERT INTO \"Notes\" (\"titol\",\"dataCreacio\",\"ultimaModificacio\",\"cos\") VALUES ('" + nom + "', '"  + dataCreacio + "', '"  + ultimaModificacio + "', '"  + cos + "')");
+
+			String nota = getUltimaNota();
+
+			for (int i = 0; i < grupsID.size(); i++) {
+				String query = "INSERT INTO \"GrupsNota\" VALUES ('" + grupsID.get(i) + "', '" + nota + "')";
+				stmt.executeUpdate(query);
+			}
+			conn.commit();
+			return nom;
+		}
+		catch(Exception e)
+		{
+			conn.rollback();
+		}
+		finally
+		{
+			conn.close();
+		}
+		return "Error";
+
+	}
+	
+	//----------------------------------------------------------------------------------------------------------------------
+	
+	/**
+	 * Insereix a la base de dades una nota a partir de un Objecte nota.
+	 * @param nota
+	 * @return
+	 * @throws SQLException
+	 */
+	public String inserirNovaNota(Nota nota) throws SQLException {
+		try
+		{
+			conn.setAutoCommit(false);
+			stmt = conn.createStatement();
+			stmt.executeUpdate("INSERT INTO \"Notes\" (\"titol\",\"dataCreacio\",\"ultimaModificacio\",\"cos\") VALUES  ('" + nota.getTitol() + "', '"  + nota.getDataPublicacio() + "', '"  + nota.getDataUltModificacio() + "', '"  + nota.getText() + "')");
+
+			String notaU = getUltimaNota();
+
+			for (int i = 0; i < grupsID.size(); i++) {
+				String query = "INSERT INTO \"GrupsNota\" VALUES ('" + grupsID.get(i) + "', '" + notaU + "')";
+				stmt.executeUpdate(query);
+			}
+
+			conn.commit();
+		}
+		catch(Exception e)
+		{
+			conn.rollback();
+			return "Error";
+		}
+		finally
+		{
+			conn.close();
+		}
+		return "Correct";
 	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.Statement;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,6 +10,7 @@ public class Main {
 		String usuariConnectat = "dgonzalo";
 		Class.forName("org.postgresql.Driver");
 		
+		//Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Component", "postgres", "dam");
 		Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/penpals", "postgres", "root");
 		connection.setAutoCommit(true);
 		if (connection!=null) System.out.println("-> Connexió establerta amb la base de dades.");
@@ -19,17 +19,14 @@ public class Main {
 		//List<String> grups = Arrays.asList("DAM1");
 		//ConnexioNotes conn = new ConnexioNotes(connection, grups);
 		ConnexioNotes conn = new ConnexioNotes(connection, usuariConnectat);
-		
-		try {
-		Statement stmt = connection.createStatement();
-		stmt.executeQuery("INSERT INTO \"GrupsNota\" VALUES (1,3)");
-		} catch (Exception ignored) {}
-		
+				
+		Thread.sleep(3000); //Afegir nota nova
 		
 		boolean hiHaNoves = conn.hiHaNotesNoves();
 		
 		if (hiHaNoves) {
-			System.out.println("--- Notes noves ---");
+			System.out.println("\nNotes noves:");
+		
 			List<Nota> llistaNotesNoves = conn.getNotesNoves();
 			for (int i=0; i<llistaNotesNoves.size(); i++) {
 				System.out.println("   - " + llistaNotesNoves.get(i).getTitol() + " - " + llistaNotesNoves.get(i).getText());
@@ -38,8 +35,19 @@ public class Main {
 		
 		else System.out.println ("\nNo hi ha notes noves");
 		
-		Nota n = conn.getNota("4");
+		Nota n = conn.getNota("\n4");
 		System.out.println(n.getTitol() + " - " + n.getText());
+		
+		Nota nota = new Nota();
+		nota.setTitol("Titol");
+		nota.setDataPublicacio("2018-3-3");
+		nota.setDataUltModificacio("2018-3-3");
+		nota.setAutor("Eduardo");
+		nota.setText("Prova Xavier");
+		
+		System.out.println(conn.inserirNovaNota(nota));
+		
+		
 		
 	}
 }
